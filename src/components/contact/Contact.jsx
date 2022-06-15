1ab171c43030bda18ae434987de9a0e25ccc9a67
eszterk90git config --global user.email eszter.kadar1@gmail.com <82053220+eszterk90@users.noreply.github.com>
2022-06-15 09:51:07 +0200

    styling improved

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,10 +6,6 @@ function Contact() {
 
   return (
     <Wrapper className='main-contact'>
-      <div className='header'>
-        <h1>Let's get in touch!</h1>
-      </div>
-     
       <Form/>
     </Wrapper>
   )

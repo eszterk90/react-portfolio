7e82387fb1fb58fb3e2eb9bc87ed64f5f1cefc45
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-08-26 16:28:05 +0200

    project descriptions added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,9 @@
 import React, {useContext} from 'react'
 import Context from '../../store/context'
 import {Link} from 'react-router-dom'
-import Calculator from '../../images/calculator.png'
-import CrwnProject from '../../images/crwn-clothing.png'
-import Agency from '../../images/x-agency.png'
+import Library from '../../images/Library.png'
+import CrwnProject from '../../images/CWRN-WEBSHOP.png'
+import Agency from '../../images/X-AGENCY.png'
 
 
 function Projects() {
@@ -22,7 +22,7 @@ function Projects() {
             </div>
             <div className="description">
                 <h5>What is this?</h5>
-                <p>We created  as a group project the frontend side of a fictive agency website, specializing in web development and online marketing. We made decisions about the project architecture and layout as a group. </p>
+                <p>In this group project we created the frontend side of a fictive agency website, specializing in web development and online marketing. I took part in implementing the project architecture using context api and wrote the code for the career, contact and about pages.</p>
                 <p>Technologies: React, SCSS, HTML5</p>
             </div>
         </div>
@@ -30,7 +30,7 @@ function Projects() {
             <div className="project_container">
                 <img src={CrwnProject} alt='project'/>
                 <div className="click_here">
-                    <a href="#" target="_about">CRWN-Webshop</a>
+                    <a href="https://crwn-webshop.netlify.app/" target="_about">CRWN-Webshop</a>
                 </div>
             </div>
             <div className="description">
@@ -41,14 +41,15 @@ function Projects() {
         </div>
         <div className="projects">
         <div className="project_container">
-            <img src={CrwnProject} alt="project"/>
+            <img src={Library} alt="project"/>
             <div className="click_here">
-                <Link to="/calculator" target="_about">CWRN Webshop</Link>
+                <a href="https://mern-library-five.vercel.app/" target="_about">Library</a>
             </div>
         </div> 
         <div className="description">
         <h5>What is this?</h5>
-        <p>This is a full-stack E-commerce app that I created by completing the Complete React Developer in 2022 on Udemy. Building this project I learnt about design patterns, how to architect my app, organize code, structure folders, and how to think about performance. Technologies: React, Firebase, SCSS, Redux, GraphQL, Typescript</p>
+        <p>In this team project we created a full-stack mockup library, in which I implemented and leveraged the logic and functionality of the code using context api, took part in writing the server side code and created the home page, profile and single book components.</p>
+        <p>Technologies: React, MongoDB, Express, Node, SCSS, HTML5</p>
         </div>
     </div>
     </div>

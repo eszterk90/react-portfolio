6240fbef069287c6b9a92b38e0be19bfef7ea2f0
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2023-01-06 15:50:43 +0100

    update skill levels

--- src/components/about/Skills.jsx
+++ src/components/about/Skills.jsx
@@ -53,62 +53,62 @@ function Skills() {
         <div className="skill skill1">
           <AiOutlineHtml5 className='skill-icon'/>
           <h4>HTML5</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div> 
         <div className="skill skill2">
           <DiCss3Full className='skill-icon'/>
           <h4>CSS</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill3">
           <DiSass className='skill-icon'/>
           <h4>SCSS</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div class="skill skill4">
           <DiJavascript1 className='skill-icon'/>
           <h4>Javascript</h4>
-          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div class="skill skill5">
           <SiTypescript className='skill-icon'/>
           <h4>Typescript</h4>
-          <div class="star"><AiOutlineStar/><AiOutlineStar/></div>
+          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill6">
           <DiReact className='skill-icon'/>
           <h4>React</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill7">
           <SiAngular className='skill-icon'/>
           <h4>Angular</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill8">
           <FaNode className='skill-icon'/>
           <h4>Node</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill9">
           <SiExpress className='skill-icon'/>
           <h4>Express</h4>
-          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div className="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill10">
           <SiMongodb className='skill-icon'/>
           <h4>MongoDB</h4>
-          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
+          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill11">
           <SiMysql className='skill-icon'/>
           <h4>MySQL</h4>
-          <div class="star"><AiOutlineStar/><AiOutlineStar/></div>
+          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
         <div className="skill skill12">
           <SiFirebase className='skill-icon'/>
           <h4>Firebase</h4>
-          <div class="star"><AiOutlineStar/><AiOutlineStar/></div>
+          <div class="star"><AiOutlineStar/><AiOutlineStar/><AiOutlineStar/></div>
         </div>
       </div>
     </div>
